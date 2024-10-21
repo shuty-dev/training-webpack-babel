0287cd4f8e922973f45c3804e6916a901db5ffe4
shutoinoue <developer1019wilshere@gmail.com>
2024-10-21 23:28:08 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  mode: "development",
+  devtool:
+    process.env.NODE_ENV === "production" ? "source-map" : "eval-source-map",
+};
